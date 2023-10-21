b3b8f388039947e6c588722e841c3a2f07059f79
ztepler <ztepler@gmail.com>
2023-10-21 23:38:09 +0300

    refactored code and removed  from test entities

--- components/tezos-l1-side/tests/base.py
+++ components/tezos-l1-side/tests/base.py
@@ -9,60 +9,80 @@ from tests.helpers.contracts import (
     ContractHelper,
 )
 from tests.helpers.utility import pkh, pack
-from typing import Type, TypeVar
+from typing import Type, TypeVar, TypedDict
+
+
+class Contracts(TypedDict):
+    proxy_deposit: ProxyRouterDeposit
+    proxy_ticketer: ProxyTicketer
+    rollup_mock: RollupMock
+    fa2: FA2
+    ticketer: Ticketer
 
 
 class BaseTestCase(SandboxedNodeTestCase):
 
     def activate_accs(self) -> None:
         # TODO: consider adding some Account abstraction with `address` property
-        self.alice = self.client.using(key='bootstrap1')
-        self.alice.reveal()
+        alice = self.client.using(key='bootstrap1')
+        alice.reveal()
 
-        self.boris = self.client.using(key='bootstrap2')
-        self.boris.reveal()
+        boris = self.client.using(key='bootstrap2')
+        boris.reveal()
 
-        self.manager = self.client.using(key='bootstrap4')
-        self.manager.reveal()
+        manager = self.client.using(key='bootstrap4')
+        manager.reveal()
+        self.accs = {
+            'alice': alice,
+            'boris': boris,
+            'manager': manager,
+        }
 
 
     def setUp(self) -> None:
         self.activate_accs()
+        manager = self.accs['manager']
 
         # Contracts deployment:
         T = TypeVar('T', bound='ContractHelper')
         def deploy_contract(cls: Type[T]) -> T:
-            opg = cls.originate_default(self.manager).send()
+            opg = cls.originate_default(manager).send()
             self.bake_block()
-            return cls.create_from_opg(self.manager, opg)
+            return cls.create_from_opg(manager, opg)
 
-        self.proxy_deposit = deploy_contract(ProxyRouterDeposit)
-        self.proxy_ticketer = deploy_contract(ProxyTicketer)
-        self.rollup_mock = deploy_contract(RollupMock)
+        proxy_deposit = deploy_contract(ProxyRouterDeposit)
+        proxy_ticketer = deploy_contract(ProxyTicketer)
+        rollup_mock = deploy_contract(RollupMock)
 
         # Tokens deployment:
         token_balances = {
-            pkh(self.alice): 1000,
-            pkh(self.boris): 1000,
-            pkh(self.manager): 1000,
+            pkh(account): 1000 for account in self.accs.values()
         }
 
-        fa2_opg = FA2.originate(self.manager, token_balances).send()
+        fa2_opg = FA2.originate(manager, token_balances).send()
         self.bake_block()
-        self.fa2 = FA2.create_from_opg(self.manager, fa2_opg)
+        fa2 = FA2.create_from_opg(manager, fa2_opg)
 
         # Deploying Ticketer with external metadata:
-        fa2_key = ( "fa2", ( self.fa2.address, 0 ) )
+        fa2_key = ( "fa2", ( fa2.address, 0 ) )
         fa2_external_metadata = {
             'decimals': pack(12, 'nat'),
             'symbol': pack('TEST', 'string'),
         }
 
         opg = Ticketer.originate_with_external_metadata(
-            self.manager,
+            manager,
             external_metadata={
                 fa2_key: fa2_external_metadata
             },
         ).send()
         self.bake_block()
-        self.ticketer = Ticketer.create_from_opg(self.manager, opg)
+        ticketer = Ticketer.create_from_opg(manager, opg)
+
+        self.contracts: Contracts = {
+            'proxy_deposit': proxy_deposit,
+            'proxy_ticketer': proxy_ticketer,
+            'rollup_mock': rollup_mock,
+            'fa2': fa2,
+            'ticketer': ticketer,
+        }
--- components/tezos-l1-side/tests/test_communication.py
+++ components/tezos-l1-side/tests/test_communication.py
@@ -19,21 +19,27 @@ class TicketerCommunicationTestCase(BaseTestCase):
         #    - as far as implicit address can't send tickets with extra data
         #      we use special proxy contract to do this
 
-        # TODO: it is probably good idea to remove `self.` from all contracts
+        boris = self.accs['boris']
+        alice = self.accs['alice']
+        fa2 = self.contracts['fa2']
+        proxy_deposit = self.contracts['proxy_deposit']
+        proxy_ticketer = self.contracts['proxy_ticketer']
+        rollup_mock = self.contracts['rollup_mock']
+        ticketer = self.contracts['ticketer']
 
         # First we check that ticketer has no tickets and no tokens:
         with self.assertRaises(KeyError):
-            self.fa2.get_balance(self.ticketer.address)
-        assert len(self.rollup_mock.get_tickets()) == 0
+            fa2.get_balance(ticketer.address)
+        assert len(rollup_mock.get_tickets()) == 0
 
         # TODO: need to create some helper to manage ticket creation / transfers:
         # TODO: need to allow to create token info from token + ticketer
         ticket = create_ticket(
-            ticketer=self.ticketer.address,
+            ticketer=ticketer.address,
             token_id=0,
             token_info={
-                'contract_address': pack(self.fa2.address, 'address'),
-                'token_id': pack(self.fa2.token_id, 'nat'),
+                'contract_address': pack(fa2.address, 'address'),
+                'token_id': pack(fa2.token_id, 'nat'),
                 'token_type': pack("FA2", 'string'),
                 'decimals': pack(12, 'nat'),
                 'symbol': pack('TEST', 'string'),
@@ -44,19 +50,20 @@ class TicketerCommunicationTestCase(BaseTestCase):
         # deposit tokens to the ticketer, set routing info to the proxy
         # and transfer ticket to the Rollup (Locker) by sending created ticket
         # to the proxy contract, which will send it to the Rollup with routing info:
-        self.alice.bulk(
-            self.fa2.using(self.alice).allow(self.ticketer.address),
-            self.ticketer.using(self.alice).deposit(self.fa2, 100),
-            self.proxy_deposit.using(self.alice).set({
+        alice.bulk(
+            fa2.using(alice).allow(ticketer.address),
+            ticketer.using(alice).deposit(fa2, 100),
+            proxy_deposit.using(alice).set({
                 'data': 'Alice L2 address'.encode('utf-8'),
-                'receiver': f'{self.rollup_mock.address}%deposit',
+                'receiver': f'{rollup_mock.address}%deposit',
             }),
-            self.alice.transfer_ticket(
+            # TODO: ticket helper may be good here: alice.transfer_ticket(ticket, 25, dest, entry)
+            alice.transfer_ticket(
                 ticket_contents = ticket['content'],
                 ticket_ty = ticket['content_type'],
                 ticket_ticketer = ticket['ticketer'],
                 ticket_amount = 25,
-                destination = self.proxy_deposit.address,
+                destination = proxy_deposit.address,
                 entrypoint = 'send',
             ),
         ).send()
@@ -68,44 +75,44 @@ class TicketerCommunicationTestCase(BaseTestCase):
         balance = get_ticket_balance(
             self.client,
             ticket,
-            self.rollup_mock.address,
+            rollup_mock.address,
         )
         self.assertEqual(balance, 25)
 
         # 2. Ticketer has FA2 tokens:
-        assert self.fa2.get_balance(self.ticketer.address) == 100
+        assert fa2.get_balance(ticketer.address) == 100
 
         # 3. Alice has L1 tickets:
         balance = get_ticket_balance(
             self.client,
             ticket,
-            pkh(self.alice),
+            pkh(alice),
         )
         self.assertEqual(balance, 75)
 
         # Then some interactions on L2 leads to outbox message creation:
         #    for example Alice send some L2 tokens to Boris and Boris decided
         #    to bridge 5 of them back to L1
-        self.rollup_mock.using(self.boris).create_outbox_message({
+        rollup_mock.using(boris).create_outbox_message({
             'ticket_id': {
                 'ticketer': ticket['ticketer'],
                 'token_id': 0,
             },
             'amount': 5,
-            'routing_data': pack(pkh(self.boris), 'address'),
+            'routing_data': pack(pkh(boris), 'address'),
         }).send()
 
         self.bake_block()
 
         # Anyone can trigger outbox message:
-        self.rollup_mock.execute_outbox_message(0).send()
+        rollup_mock.execute_outbox_message(0).send()
         self.bake_block()
 
         # Boris should have now L1 tickets too:
         balance = get_ticket_balance(
             self.client,
             ticket,
-            pkh(self.boris),
+            pkh(boris),
         )
         self.assertEqual(balance, 5)
 
@@ -113,24 +120,24 @@ class TicketerCommunicationTestCase(BaseTestCase):
         balance = get_ticket_balance(
             self.client,
             ticket,
-            self.rollup_mock.address,
+            rollup_mock.address,
         )
         self.assertEqual(balance, 20)
 
         # Boris unpacks some L1 tickets to get back some FA2 tokens
-        boris_tokens_before_burn = self.fa2.get_balance(pkh(self.boris))
+        boris_tokens_before_burn = fa2.get_balance(pkh(boris))
 
-        self.boris.bulk(
-            self.proxy_ticketer.using(self.boris).set({
-                'data': pkh(self.boris),
-                'receiver': f'{self.ticketer.address}%release',
+        boris.bulk(
+            proxy_ticketer.using(boris).set({
+                'data': pkh(boris),
+                'receiver': f'{ticketer.address}%release',
             }),
-            self.boris.transfer_ticket(
+            boris.transfer_ticket(
                 ticket_contents=ticket['content'],
                 ticket_ty=ticket['content_type'],
                 ticket_ticketer=ticket['ticketer'],
                 ticket_amount=2,
-                destination=self.proxy_ticketer.address,
+                destination=proxy_ticketer.address,
                 entrypoint='send',
             )
         ).send()
@@ -140,12 +147,12 @@ class TicketerCommunicationTestCase(BaseTestCase):
         balance = get_ticket_balance(
             self.client,
             ticket,
-            pkh(self.boris),
+            pkh(boris),
         )
         self.assertEqual(balance, 3)
 
         # Boris should have more FA2 tokens now:
-        boris_tokens_after_burn = self.fa2.get_balance(pkh(self.boris))
+        boris_tokens_after_burn = fa2.get_balance(pkh(boris))
         self.assertEqual(
             boris_tokens_after_burn,
             boris_tokens_before_burn + 2

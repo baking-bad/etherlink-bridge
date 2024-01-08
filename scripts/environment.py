4142cedbf27ac3f67934bd46c8f1af8e2eec4fd1
ztepler <ztepler@gmail.com>
2024-01-08 17:34:11 +0300

    added private keys to the repo

--- scripts/environment.py
+++ scripts/environment.py
@@ -33,16 +33,28 @@ def init_wallets() -> None:
         return
 
     variables = [
-        Variable('L1_PRIVATE_KEY', is_secret=True),
+        Variable(
+            'L1_PRIVATE_KEY',
+            default='edsk2nu78mRwg4V5Ka7XCJFVbVPPwhry8YPeEHRwzGQHEpGAffDvrH',
+            is_secret=True,
+        ),
         # TODO: it is possible to get L1_PUBLIC_KEY_HASH from L1_PRIVATE_KEY
-        Variable('L1_PUBLIC_KEY_HASH'),
+        Variable('L1_PUBLIC_KEY_HASH', default='tz1YG6P2GTQKFpd9jeuESam2vg6aA9HHRkKo'),
         Variable('L1_RPC_URL', default='https://rpc.tzkt.io/nairobinet/'),
         Variable('L1_ROLLUP_ADDRESS', default='sr1Cz5HkqyC8ozTYbDij8YmseAAG3SgZtpRb'),
-        Variable('L2_PRIVATE_KEY', is_secret=True),
+        Variable(
+            'L2_PRIVATE_KEY',
+            default='8636c473b431be57109d4153735315a5cdf36b3841eb2cfa80b75b3dcd2d941a',
+            is_secret=True,
+        ),
         # TODO: it is possible to get L2_PUBLIC_KEY from L2_PRIVATE_KEY
-        Variable('L2_PUBLIC_KEY'),
+        Variable('L2_PUBLIC_KEY', default='0xBefD2C6fFC36249ebEbd21d6DF6376ecF3BAc448'),
         # TODO: maybe it is better not to ask for L2_MASTER_KEY
-        Variable('L2_MASTER_KEY', is_secret=True),
+        Variable(
+            'L2_MASTER_KEY',
+            default='9722f6cc9ff938e63f8ccb74c3daa6b45837e5c5e3835ac08c44c50ab5f39dc0',
+            is_secret=True,
+        ),
         Variable('L2_RPC_URL', default='https://etherlink.dipdup.net'),
         Variable(
             'L2_KERNEL_ADDRESS', default='0x0000000000000000000000000000000000000000'

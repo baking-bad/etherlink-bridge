581d91535864b749ff752516d7f16f587520a1e4
ztepler <ztepler@gmail.com>
2024-01-12 14:56:05 +0300

    added build_tezos_contracts script

--- scripts/tezos.py
+++ scripts/tezos.py
@@ -19,6 +19,8 @@ from tezos.tests.helpers.proof import (
     get_proof as get_proof_from_rpc,
     Proof,
 )
+import subprocess
+import os
 
 
 def get_client() -> PyTezosClient:
@@ -319,6 +321,79 @@ def execute_outbox_message(
     ).send()
     manager.wait(opg)
     print(f'Succeed, transaction hash: {opg.hash()}')
+
+
+@click.command()
+@click.option(
+    '--ligo-version',
+    default='0.70.1',
+    help='LIGO compiler version used to compile Tezos side contracts, default: 0.70.1',
+)
+def build_contracts(
+    ligo_version: str,
+) -> None:
+    """Compiles Tezos side contracts"""
+
+    def compile_contract(
+        contract_path: str,
+        module_name: str,
+        output_path: str,
+    ) -> None:
+        print(f'- Compiling {module_name} contract')
+        cwd = os.path.join(os.getcwd(), 'tezos')
+        result = subprocess.run(
+            [
+                'docker',
+                'run',
+                '--rm',
+                '-v',
+                f'{cwd}:{cwd}',
+                '-w',
+                cwd,
+                f'ligolang/ligo:{ligo_version}',
+                'compile',
+                'contract',
+                contract_path,
+                '-m',
+                module_name,
+                '-o',
+                output_path,
+            ],
+            # check=True,
+            cwd=cwd,
+        )
+        if result.stdout:
+            print(result.stdout)
+        if result.stderr:
+            print(result.stderr)
+
+    if not os.path.exists('tezos/build'):
+        os.makedirs('tezos/build')
+
+    # TODO: consider removing everything from build directory before compiling
+
+    print('Compiling Tezos side contracts:')
+    compile_contract(
+        'contracts/ticket-helper/ticket-helper.mligo',
+        'TicketHelper',
+        'build/ticket-helper.tz',
+    )
+    compile_contract(
+        'contracts/rollup-mock/rollup-mock.mligo',
+        'RollupMock',
+        'build/rollup-mock.tz',
+    )
+    compile_contract(
+        'contracts/ticketer/ticketer.mligo',
+        'Ticketer',
+        'build/ticketer.tz',
+    )
+    compile_contract(
+        'contracts/router.mligo',
+        'Router',
+        'build/router.tz',
+    )
+    print('Done')
 
 
 # TODO: rework these functions:

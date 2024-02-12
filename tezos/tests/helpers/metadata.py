ccc75d4ee48118827950c7b3e92ca3ce44eefbba
ztepler <ztepler@gmail.com>
2024-02-12 17:37:33 +0300

    bumped contracts version

--- tezos/tests/helpers/metadata.py
+++ tezos/tests/helpers/metadata.py
@@ -11,7 +11,7 @@ class Metadata:
     """Helper to create metadata for the contracts"""
 
     template = {
-        'version': '0.2.0',
+        'version': '0.3.0',
         'name': 'Etherlink Bridge',
         'description': 'The Etherlink Bridge consists of contracts designed for communication between Tezos and Etherlink rollup.',
         'interfaces': ['TZIP-016'],

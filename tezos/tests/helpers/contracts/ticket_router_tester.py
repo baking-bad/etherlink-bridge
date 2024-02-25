b20e8ab4927cf403ea911ac48e65020bfca5132c
ztepler <ztepler@gmail.com>
2024-02-25 04:27:31 +0300

    syncronized TicketRouterTester tests helper, added set entrypoint wrapper

--- tezos/tests/helpers/contracts/ticket_router_tester.py
+++ tezos/tests/helpers/contracts/ticket_router_tester.py
@@ -8,7 +8,10 @@ from tezos.tests.helpers.utility import (
 from pytezos.operation.group import OperationGroup
 from os.path import join
 from tezos.tests.helpers.metadata import Metadata
-from typing import Optional
+from typing import (
+    Optional,
+    Any,
+)
 
 
 class TicketRouterTester(ContractHelper):
@@ -17,8 +20,11 @@ class TicketRouterTester(ContractHelper):
         """Deploys TicketRouterTester"""
 
         storage = {
-            'rollup': DEFAULT_ADDRESS,
-            'routing_info': bytes(40),
+            'internal_call': {
+                'target': DEFAULT_ADDRESS,
+                'entrypoint': {'default': None},
+                'xtz_amount': 0,
+            },
             'metadata': Metadata.make_default(
                 name='Ticket Router Tester',
                 description='The Ticket Router Tester is a helper contract allowing mint, deposit and withdraw tickets testing for the Etherlink Bridge contracts.',
@@ -47,3 +53,27 @@ class TicketRouterTester(ContractHelper):
             content=micheline_content_to_tuple(micheline_content),
             amount=amount,
         )
+
+    def set(
+        self,
+        target: str,
+        entrypoint: dict[str, Any],
+        xtz_amount: int = 0,
+    ) -> OperationGroup:
+        """Sets the internal call parameters that will be used to wrap
+        ticket at the next `mint` calls and redirect tickets for the
+        next `default` or `withdraw` calls.
+
+        Entrypoint is one of the following:
+            - `default` can be used to send tickets to the implicit address
+            - `routerWithdraw` can be used to test Router.withdraw entrypoint
+                and unwrap tickets utilizing Ticketer.withdraw entrypoint
+            - `rollupDeposit` to be used as TicketHelper without restrictions
+                to test rollup operations
+        """
+
+        return self.contract.set(
+            target=target,
+            entrypoint=entrypoint,
+            xtz_amount=xtz_amount,
+        ).with_amount(xtz_amount)

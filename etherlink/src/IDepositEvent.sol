1f814e7b41d132f4526ceca176ba4b7e203b6eac
ztepler <ztepler@gmail.com>
2024-04-03 17:58:30 +0300

    updated Etherlink side documentations to follow NatSpec [q:23]

--- etherlink/src/IDepositEvent.sol
+++ etherlink/src/IDepositEvent.sol
@@ -2,11 +2,17 @@
 pragma solidity >=0.8.21;
 
 /**
- * @dev Interface of the contract which can emit Deposit event.
+ * @notice Interface for emitting Deposit events in an L1 to L2 token bridge.
  */
 interface IDepositEvent {
     /**
-     * @dev Emitted when succesful deposit is made.
+     * @notice Indicates a deposit from L1 to L2.
+     * @param ticketHash Unique identifier for the deposited ticket.
+     * @param ticketOwner L2 owner who receiving the ticket.
+     * @param receiver L2 address receiving the tokens.
+     * @param amount Amount of tokens deposited.
+     * @param inboxLevel The level at which the deposit was included in the rollup inbox.
+     * @param inboxMsgId Inbox message identifier in the rollup inbox at the given level.
      */
     event Deposit(
         uint256 indexed ticketHash,

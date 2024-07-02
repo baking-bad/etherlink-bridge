4076a8b6f6ee9f5dcee37cb95a30d945544be16c
Igor Sereda <sereda.igor.s@gmail.com>
2024-07-02 06:57:15 +0300

    Refactoring of the Test Token bootstrapping service

--- scripts/bootstrap/dto.py
+++ scripts/bootstrap/dto.py
@@ -1,19 +1,30 @@
-from typing import Any
-
 from pydantic import BaseModel
 
+from scripts.bootstrap.const import DEFAULT_TOKEN_ID
 from scripts.helpers.contracts import Ticketer
 
+
+class TicketerParamsDTO(BaseModel):
+    address_bytes_hex: str
+    content_bytes_hex: str
 
 class TicketerDTO(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
-    metadata: dict[str, Any]
     ticketer: Ticketer
-    ticketer_params: dict[str, str]
+    ticketer_params: TicketerParamsDTO
     ticket_hash: int
 
+class TokenMetadataDTO(BaseModel):
+    name: str
+    symbol: str
+    decimals: int = DEFAULT_TOKEN_ID
+
+class TokenInfoDTO(BaseModel):
+    metadata: TokenMetadataDTO
+    standard: str
+    supply: int
 
 class UserInputDTO(BaseModel):
     is_mainnet: bool

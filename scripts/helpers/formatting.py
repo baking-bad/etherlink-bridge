6320ecdb37ea79baa7d445721f189b02c8e49d45
ztepler <ztepler@gmail.com>
2024-07-31 14:51:06 +0300

    added format_token_info helper

--- scripts/helpers/formatting.py
+++ scripts/helpers/formatting.py
@@ -1,4 +1,5 @@
 import click
+from scripts.helpers.contracts import TokenHelper
 
 
 def accent(msg: str) -> str:
@@ -15,3 +16,25 @@ def echo_variable(prefix: str, name: str, value: str) -> None:
 
 def format_int(number: int) -> str:
     return f'{number:0,d}'.replace(',', '_')
+
+
+def format_token_info(token: TokenHelper) -> str:
+    # TODO: consider moving this logic to the Token?
+    token_dict = token.as_dict()
+    if 'fa2' in token_dict:
+        return (
+            accent('FA2')
+            + ' token, address: '
+            + wrap(accent(token.address))
+            + ', id: '
+            + wrap(accent(str(token.token_id)))
+        )
+    elif 'fa12' in token_dict:
+        return accent('FA1.2') + 'token, address' + wrap(accent(token.address))
+    else:
+        return (
+            'Undefined token type, address: '
+            + wrap(accent(token.address))
+            + ', id: '
+            + wrap(accent(str(token.token_id)))
+        )

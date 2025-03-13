e5c3befdb50a11511f10641286fbf9c7d650036d
ztepler <ztepler@gmail.com>
2025-03-13 17:37:22 +0000

    Added read_ticket method for native xtz ticketer

--- scripts/helpers/contracts/exchanger.py
+++ scripts/helpers/contracts/exchanger.py
@@ -1,9 +1,13 @@
 from os.path import join
+from typing import Optional
 
 from pytezos.client import PyTezosClient
 from pytezos.operation.group import OperationGroup
 
+from scripts.helpers.addressable import Addressable
 from scripts.helpers.contracts.contract import ContractHelper
+from scripts.helpers.ticket_content import TicketContent
+from scripts.helpers.ticket import Ticket
 from scripts.helpers.utility import get_build_dir
 from scripts.helpers.utility import originate_from_file
 
@@ -19,3 +23,18 @@ class Exchanger(ContractHelper):
         filename = join(get_build_dir(), "exchanger.tz")
         storage: dict = {}
         return originate_from_file(filename, client, storage)
+
+    def read_ticket(
+        self,
+        owner: Optional[Addressable] = None,
+    ) -> Ticket:
+        """Returns ticket with Exchanger's content that can be used in
+        `ticket_transfer` call. Amount is set to the client's balance."""
+
+        owner = owner or self.client
+        return Ticket.create(
+            client=self.client,
+            owner=owner,
+            ticketer=self.address,
+            content=TicketContent(0, None),
+        )
